4cad33794290f4cfeb5acff62ab6bcd5b236a5b5
aspieslechner <aspieslechner@gvcgroup.com>
2019-01-06 14:28:00 +0100

    remove other bloat
    removed jetpack, woocommerce and wpcom features as I dont need them

--- starter-theme/functions.php
+++ starter-theme/functions.php
@@ -150,17 +150,3 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-  require get_template_directory() . '/inc/jetpack.php';
-}
-
-/**
- * Load WooCommerce compatibility file.
- */
-if ( class_exists( 'WooCommerce' ) ) {
-  require get_template_directory() . '/inc/woocommerce.php';
-}
