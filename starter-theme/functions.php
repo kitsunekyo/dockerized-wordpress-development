274ca7156af6a1534ba3945eca02aacba79776cb
aspieslechner <aspieslechner@gvcgroup.com>
2019-01-06 14:29:13 +0100

    remove custom-header
    not needed in my starter

--- starter-theme/functions.php
+++ starter-theme/functions.php
@@ -132,11 +132,6 @@ function starter_theme_scripts() {
 add_action( 'wp_enqueue_scripts', 'starter_theme_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
